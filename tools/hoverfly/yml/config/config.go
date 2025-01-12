c699e62d5a506d3a130a0303bdb3fdc987b1806c
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-01-12 22:35:55 +0200

    code: config high level representation
    Signed-off-by: Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>

--- tools/hoverfly/yml/config/config.go
+++ tools/hoverfly/yml/config/config.go
@@ -2,33 +2,58 @@ package config
 
 import "net/http"
 
+// Config represents the main configuration interface.
 type Config interface {
-	Routes() []Route
+	Settings() Settings
 	Patterns() []Pattern
+	Rules() []Rule
 }
 
-type Route interface {
+// Settings represents configuration settings such as debug mode or case sensitivity.
+type Settings interface {
+	CaseSensitive() bool
+	Debug() bool
+}
+
+// Pattern represents a general transformation rule.
+type Pattern interface {
+	Type() string
+	Formats() []string
+	Pattern() *string
+	KeepChars() *int
+	ReplaceWith() string
+}
+
+// Rule represents an API route and its associated rules.
+type Rule interface {
 	Path() string
 	Methods() []string
 	Request() Request
 	Response() Response
 }
 
+// Request represents the configuration of a request.
 type Request interface {
-	Body() string
+	Body() RequestBody
 	Headers() http.Header
 	AdditionalConfig() map[string]any
 }
 
+// RequestBody represents specific body rules in a request.
+type RequestBody interface {
+	Fields() map[string]FieldRule
+}
+
+// Response represents the configuration of a response.
 type Response interface {
-	Body() string
-	Headers() http.Header
-	AdditionalConfig() map[string]any
+	TemplateMatchedFields() bool
 }
 
-type Pattern interface {
+// FieldRule defines rules for a specific field in a request body.
+type FieldRule interface {
 	Type() string
-	Formats() []string
 	Pattern() *string
+	KeepChars() *int
+	Formats() []string
 	ReplaceWith() string
 }
